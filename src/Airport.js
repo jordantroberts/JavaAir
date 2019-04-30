3d2a6607ab0129afa8b0df692fc4d8b7ae96eafe
Chenbo Yi <yichenbo@outlook.com>
2019-04-30 15:59:43 +0100

    inject dependency of weather to airport

--- src/Airport.js
+++ src/Airport.js
@@ -1,4 +1,4 @@
-var Airport = function(){this.planes = [], this.weather = new Weather()};
+var Airport = function(weather = new Weather() ){this.planes = [], this.weather = weather};
 
 Airport.prototype.land = function(plane) {
   this.planes.push(plane);
@@ -7,7 +7,7 @@ Airport.prototype.land = function(plane) {
 Airport.prototype.takeoff = function(plane) {
   console.log(this.weather.forecast())
   if (this.weather.forecast() == 'stormy') {
-    throw "Too stormy to take off";
+    throw new Error( "Too stormy to take off");
   }
   this.planes.pop();
 };
